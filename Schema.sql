cd9587217cd9e22098d67bea5c9ae61609e2f4d5
Anil Yadav <rebelanil885@gmail.com>
2024-12-11 22:42:08 +0530

    ejs newuse form data passed to sql DB succesfully

--- Schema.sql
+++ Schema.sql
@@ -3,9 +3,9 @@ SHOW TABLES; -- TO EXECUTE THIS CODE WE FIRST GO TO CLI THEN USE DB WHICH WE WAN
 -- this is the fourth way to code or run query of mysql rather than workbench 
 -- these code will execute after 'source Schema.sql' in CLI
 CREATE TABLE blogData(
-    id INT PRIMARY KEY UNIQUE,
-    username VARCHAR(30) UNIQUE,
-    email VARCHAR(30) NOT NULL,
-    password VARCHAR(20) NOT NULL
+    id VARCHAR(200) PRIMARY KEY UNIQUE,
+    username VARCHAR(300) UNIQUE,
+    email VARCHAR(300) NOT NULL,
+    password VARCHAR(220) NOT NULL
 );
 SHOW TABLES;
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const { fakerDE: faker } = require('@faker-js/faker');
+const { fakerDE: faker, da } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+//body parser for holding template newuser data 
+
+const bodyParser = require("body-parser");
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Get connection to MySQL database
 const connection = mysql.createConnection({
@@ -8,34 +19,92 @@ const connection = mysql.createConnection({
     database: 'blog',
     password: 'serveranil'
 });
+//setting ejs path for views
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
+
+//to generate random data 
+//to access data we changed value-pair to array
+let GetRandomData = () => {
+    return [
+        faker.string.uuid(),
+        faker.internet.username(), // before version 9.1.0, use userName()
+        faker.internet.email(),
+        faker.internet.password(),
+    ];//for arr we only keep value not key
+}
+let data = [];
+for (let i = 0; i <= 100; i++) {
+    data.push(GetRandomData());
+}
+
+//routes 
+//home route - to show no.of users in db
+app.get("/", (req, res) => {
+
+    //query to show only no.of user in db
+    let q1 = "SELECT count(*) FROM blogData"; //while home route it pass query to sql as we connection with sql
+    try {
+        connection.query(q1, (err, result) => {
+            if (err) throw err; // Handle database error
+            // console.log(result[0]["count(*)"]); // Output the result
+            let count = result[0]["count(*)"];
+            res.render("home.ejs",{count});
+        });
+    } catch (e) {
+        res.send("some error in DB");
+    }
+
+})
+
+//to get new user data  with data.ejs template
+app.get("/newUser",(req,res)=>{
+   res.render("newUser.ejs");
+
+})
+//post req for holding the data and pass to sql database
+app.post("/",(req,res)=>{
+    let N_User = [[req.body.id,req.body.username,req.body.email,req.body.password]];
+    console.log(N_User);
+    //to pass into sql DB
+  let q2 = "INSERT INTO blogData (id,username,email,password) VALUES ? ";
+    try {
+            connection.query(q2, [N_User], (err, result) => {
+                if (err) throw err; // Handle database error
+                console.log(result); // Output the result
+                res.redirect("/");
+            });
+        } catch (e) {
+            res.send("some error in sql query");
+        }
+    
+    
+})
+
+
+// console.log(data);
 // Correct query to show tables in the database
 // inserting data with this query
-let q = "INSERT INTO blogData (id,username,email,password) VALUES ?"; //sql query we pass to connection
 //in form of array we insert in values i.e 
-const blogData = [["125", "anilyadav45@5", "mahiyadav42@gmail5.com", "76453Anil5"], ["126", "anilyadav45@6", "mahiyadav42@gmail6.com", "76453Anil6"]];
+// const blogData = [["125", "anilyadav45@5", "mahiyadav42@gmail5.com", "76453Anil5"], ["126", "anilyadav45@6", "mahiyadav42@gmail6.com", "76453Anil6"]];
 
 //we have to keep change it for insert each time so now we use faker data generator to learn more as we mentioned that id should be unique so while inserting it should be unique to previous one
-try {
-    connection.query(q, [blogData], (err, result) => {
-        if (err) throw err; // Handle database error
-        console.log(result); // Output the result
-        console.log(result.length);
-        console.log(result[0]);
-    });
-} catch (e) {
-    console.log("Error occurred: " + e);
-}
+// let q = "INSERT INTO blogData (id,username,email,password) VALUES ?"; //sql query we pass to connection
+// try {
+//     connection.query(q, [data], (err, result) => {
+//         if (err) throw err; // Handle database error
+//         console.log(result); // Output the result
+//         // console.log(result.length);
+//         // console.log(result[0]);
+//     });
+// } catch (e) {
+//     console.log("Error occurred: " + e);
+// }
 
 //stop connection after execution
-connection.end();
+// connection.end();
+app.listen("8080", (req, res) => {
+    console.log("app is listening on port : 8080");
+})
 
-let GetRandomData = () => {
-    return {
-        userId: faker.string.uuid(),
-        username: faker.internet.username(), // before version 9.1.0, use userName()
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-    };
-}
-console.log(GetRandomData());
