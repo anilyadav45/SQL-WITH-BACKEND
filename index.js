2f98c2b938494c72dd59542b471278de94160fff
Anil Yadav <rebelanil885@gmail.com>
2024-12-13 22:24:28 +0530

    route to edit username

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const connection = mysql.createConnection({
     password: 'serveranil'
 });
 //setting ejs path for views
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 
 //to generate random data 
@@ -50,7 +50,7 @@ app.get("/", (req, res) => {
             if (err) throw err; // Handle database error
             // console.log(result[0]["count(*)"]); // Output the result
             let count = result[0]["count(*)"];
-            res.render("home.ejs",{count});
+            res.render("home.ejs", { count });
         });
     } catch (e) {
         res.send("some error in DB");
@@ -59,68 +59,67 @@ app.get("/", (req, res) => {
 })
 
 //to get new user data  with data.ejs template
-app.get("/newUser",(req,res)=>{
-   res.render("newUser.ejs");
+app.get("/newUser", (req, res) => {
+    res.render("newUser.ejs");
 
 })
 //post req for holding the data and pass to sql database
-app.post("/",(req,res)=>{
-    let N_User = [[req.body.id,req.body.username,req.body.email,req.body.password]];
+app.post("/", (req, res) => {
+    let N_User = [[req.body.id, req.body.username, req.body.email, req.body.password]];
     console.log(N_User);
     //to pass into sql DB
-  let q2 = "INSERT INTO blogData (id,username,email,password) VALUES ? ";
+    let q2 = "INSERT INTO blogData (id,username,email,password) VALUES ? ";
     try {
-            connection.query(q2, [N_User], (err, result) => {
-                if (err) throw err; // Handle database error
-                console.log(result); // Output the result
-                res.redirect("/");
-            });
-        } catch (e) {
-            res.send("some error in sql query");
-        }
-    
-    
+        connection.query(q2, [N_User], (err, result) => {
+            if (err) throw err; // Handle database error
+            console.log(result); // Output the result
+            res.redirect("/");
+        });
+    } catch (e) {
+        res.send("some error in sql query");
+    }
+
+
 })
 
 //show route - to see all users
-app.get("/users",(req,res)=>{
-  
+app.get("/users", (req, res) => {
+
 
     let q3 = "SELECT * FROM blogData";
     try {
-            connection.query(q3, (err, users) => {
-                if (err) throw err; // Handle database error
-                console.log(users); // Output the result
-                res.render("showusers.ejs",{users});
-            });
-        } catch (e) {
-            res.send("some error in sql query");
-        }
-    
+        connection.query(q3, (err, users) => {
+            if (err) throw err; // Handle database error
+            console.log(users); // Output the result
+            res.render("showusers.ejs", { users });
+        });
+    } catch (e) {
+        res.send("some error in sql query");
+    }
+
 
 })
 
 //route to edit username
-app.get("/users/:id/edit",(req,res)=>{
+app.get("/users/:id/edit", (req, res) => {
 
-    let {id} = req.params;
+    let { id } = req.params;
     let q4 = `SELECT * FROM blogData WHERE id='${id}'`;
-    console.log(id);
     try {
-            connection.query(q4, (err, users) => {
-                if (err) throw err; // Handle database error
-                let {id} = req.params;
-                console.log(id);
-                res.render("edit.ejs",{users});
-            });
-        } catch (e) {
-            res.send("some error in sql query");
-        }
+        connection.query(q4, (err, users) => {
+            if (err) throw err; // Handle database error
+            let { id } = req.params;
+            console.log(id);
+            console.log(users[0].username);
+            res.render("edit.ejs", { users });
+        });
+    } catch (e) {
+        res.send("some error in sql query");
+    }
 
 })
-app.post("/users/:id",(req,res)=>{
-    
-})
+
+
 // console.log(data);
 // Correct query to show tables in the database
 // inserting data with this query
